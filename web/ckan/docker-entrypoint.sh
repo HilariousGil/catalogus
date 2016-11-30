da89e22d1bafa0187cf34fc790de82d029a0db8f
Thijs Creemers <thijs.creemers@gmail.com>
2016-11-30 13:18:13 +0100

    Ckan setup

--- web/ckan/docker-entrypoint.sh
+++ web/ckan/docker-entrypoint.sh
@@ -2,6 +2,6 @@
 echo Building db
 
 cd /src/ckan
-paster db init -c /app/config.ini
-paster db upgrade -c /app/config.ini
+#paster db init -c /app/config.ini
+#paster db upgrade -c /app/config.ini
 paster serve /app/config.ini
