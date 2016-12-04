ea6bbd69fba667f870b906a25d4445d78b3759c3
Thijs Creemers <thijs.creemers@gmail.com>
2016-12-04 12:31:40 +0100

    changed setup and added a downgraded version of solr (v4.7)

---
+++ solr/docker-entrypoint.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo Starting SOLR
+java -jar start.jar
--- web/ckan/docker-entrypoint.sh
+++ web/ckan/docker-entrypoint.sh
@@ -4,4 +4,7 @@ echo Building db
 cd /src/ckan
 #paster db init -c /app/config.ini
 #paster db upgrade -c /app/config.ini
+
+#pg_restore -d ckan -h database_ckan -p 5432 -U ckan /data/dump.pgsql
+#paster search-index rebuild -c /app/config.ini
 paster serve /app/config.ini
