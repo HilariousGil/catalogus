ffb619d450da9edc33da53c899b0d362814a4cf6
Thijs Creemers <thijs.creemers@gmail.com>
2017-02-21 14:01:41 +0100

    remove default configure stuff from plugin

--- web/ckanext-iauthfunctions/ckanext/iauthfunctions/plugin.py
+++ web/ckanext-iauthfunctions/ckanext/iauthfunctions/plugin.py
@@ -1,24 +1,14 @@
+import os
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from ckan.plugins.toolkit import get_action
-
-import pylons
 import jwt
+import pylons
+
 
 class IauthfunctionsPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IAuthenticator)
     plugins.implements(plugins.IAuthFunctions)
-    # plugins.implements(plugins.IPermissionLabels)
-
-    # IConfigurer
-
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'iauthfunctions')
-
-    # IAuthenticator
 
     def login(self):
         """
@@ -35,44 +25,17 @@ class IauthfunctionsPlugin(plugins.SingletonPlugin):
         user = 'anonymous'
         if pylons.session.get('Authorization'):
             jwt_payload = jwt.decode(
-                pylons.session.get('Authorization'), 'secret', algorithms=['HS256'])
-            user = jwt_payload.get('uid')
+                pylons.session.get('Authorization'),
+                os.getenv('JWT_SHARED_SECRET_KEY', 'insecure'),
+                algorithms=['HS256']
+            )
+            user = jwt_payload.get('authz')
         if user:
-            plugins.toolkit.c.user = user
+            plugins.toolkit.c.user = 'authenticated'
 
-
-    # IAuthFunctions
-
-    def get_auth_funcytions(self):
+    def get_auth_functions(self):
+        """
+        IAuthFunctions
+        :return:
+        """
         pass
-
-    # # IPermissionLabels
-    # def get_dataset_labels(self, dataset_obj):
-    #     """
-    #     Use dataclassificatie field
-    #
-    #     :param dataset_obj:
-    #     :return:
-    #     """
-    #     labels = u'public'
-    #     if dataset_obj.dataclassificatie == 'closed':
-    #         labels = u'closed'
-    #     elif dataset_obj.dataclassificatie == 'closed_plus':
-    #         labels = u'closed_plus'
-    #
-    #         return [labels, ]
-    #
-    #     return super(IauthfunctionsPlugin, self).get_dataset_labels(dataset_obj)
-    #
-    # def get_user_dataset_labels(self, user_obj):
-    #     """
-    #     Use dataclassificatie field
-    #     :param user_obj:
-    #     :return:
-    #     """
-    #     labels = super(IauthfunctionsPlugin, self).get_user_dataset_labels(user_obj)
-    #     if user_obj:
-    #         orgs = get_action(u'organization_list_for_user')(
-    #             {u'user': user_obj.id}, {u'permission': u'admin'})
-    #         labels.extend(u'admin-%s' % o['id'] for o in orgs)
-    #     return labels
